e721e37f7d04b4f871cf674a6b5e2be43fdbef2f
Evelyn Rodrigues Alves de Souza <157848082+Evelynrodrigues28@users.noreply.github.com>
2024-03-06 20:06:56 -0300

    Add files via upload

---
+++ models.js
@@ -0,0 +1,12 @@
+let modelsJson = [
+    {id:1, name:'Carne de panela com cenoura', img:'img/carne de panela com cenoura.jpg', price:[18.99, 20.99, 24.99], sizes:['300 gramas', '400 gramas', '500 gramas']},
+    {id:2, name:'Carne moída com alho poró', img:'img/carne moida com alho poro.jpg',price:[18.99, 20.99, 24.99], sizes:['300 gramas', '400 gramas', '500 gramas']},
+    {id:3, name:'Frango xadrez', img:'img/marmitas fitness.jpg', price:[18.99, 20.99, 24.99], sizes:['300 gramas', '400 gramas', '500 gramas']},
+    {id:4, name:'Peito de frango ao forno', img:'img/Frango assado ao limao.jpg',price:[18.99, 20.99, 24.99], sizes:['300 gramas', '400 gramas', '500 gramas']},
+    {id:5, name:'Iscas de frango com alho poró e tomate cereja', img:'img/marmitas fitness.jpg', price:[18.99, 20.99, 24.99], sizes:['300 gramas', '400 gramas', '500 gramas']},
+    {id:6, name:'Strogonoff de frango', img:'img/Strogonoff de carne de soja.jpg', price:[18.99, 20.99, 24.99], sizes:['300 gramas', '400 gramas', '500 gramas']},
+    {id:7, name:'Iscas de carne com pimentões coloridos', img:'img/isca de carne com pimentoes.jpg', price:[18.99, 20.99, 24.99], sizes:['300 gramas', '400 gramas', '500 gramas']},
+    {id:8, name:'Strogonoff de carne', img:'img/marmitas fitness.jpg', price:[18.99, 20.99, 24.99], sizes:['300 gramas', '400 gramas', '500 gramas']},
+    {id:9, name:'Strogonoff de carne de soja', img:'img/Strogonoff de carne de soja.jpg', price:[18.99, 20.99, 24.99], sizes:['300 gramas', '400 gramas', '500 gramas']},
+    {id:10, name:'Cogumelos (shimeji e shitake) na manteiga e cebolinha', img:'img/mix de cogumelos com ora pro nobis.jpg', price:[17.99, 19.99, 23.99], sizes:['300 gramas', '400 gramas', '500 gramas']},
+];
